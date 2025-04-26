c8560d136b3d0f9bf2e4660cf98a48f067ceab09
shubhamvraut <shubhamvraut1995@gmail.com>
2025-04-26 23:24:25 +0530

    dockerfile added an requirements file also

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from fastapi import  FastAPI
-app = FastAPI()
-
-@app.get("/")
-def index():
-    return {"message":"Hello World"}
-
 from fastapi import FastAPI, HTTPException
 import psycopg2
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import os
 from dotenv import load_dotenv
 
@@ -18,33 +11,18 @@ app = FastAPI()
 # ENV values
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-@app.get("/init/table")
-def create_users_table():
-    print("11")
-    try:
-        print(DATABASE_URL)
-        conn = psycopg2.connect(DATABASE_URL)
-        cursor = conn.cursor()
+@app.get("/")
+def index():
+    return {"message": "Swapnil Bhau kay karu rayle"}
 
-        # Create table
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(100) NOT NULL,
-            email VARCHAR(100) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            phone VARCHAR(20),
-            address TEXT,
-            city VARCHAR(50),
-            state VARCHAR(50),
-            country VARCHAR(50),
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );
-        """)
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return {"message": "Table 'users' created successfully."}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail={f"str(e)={str(e)}, message={e.__str__()}"})
+@app.get("/create")
+def create():
+    return {"message":"create successfully"}
+
+@app.get("/get")
+def create():
+    return {"message":"fetch successfully"}
+
+@app.get("/update")
+def create():
+    return {"message":"update successfully"}
